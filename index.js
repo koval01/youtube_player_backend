8800d24e8a40d94f49c1d18922cf48088fd66fed
Yaroslav Koval <yaroslav@koval.page>
2023-07-24 16:58:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
+const { promisify } = require('util');
+
 const request = require('request')
 const compression = require('compression')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
 const express = require('express')
-const Redis = require("ioredis")
+const redis = require("ioredis")
 const youtubedl = require('youtube-dl-exec')
 
 const app = express()
@@ -16,6 +18,9 @@ app.set('trust proxy', 2)
 app.use(express.json())
 app.use(compression())
 app.use(cors())
+
+const getRedisAsync = promisify(redis.get).bind(redis)
+const setRedisAsync = promisify(redis.set).bind(redis)
 
 const apiLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
@@ -28,98 +33,89 @@ const apiLimiter = rateLimit({
 })
 app.use(apiLimiter)
 
-app.post('/getVideo', async (req, resp) => {
-    let json_body = req.body
+const builder_ = (cur, audio = false) => {
+    let result = {
+        format: cur.format,
+        url: cur.url
+    }
+    if (audio) {
+        result.acodec = cur.acodec
+        result.ext = cur.ext
+    } else {
+        result.acodec = cur.acodec
+        result.vcodec = cur.vcodec
+        result.video_ext = cur.video_ext
+        result.fps = cur.fps
+    }
+    return result
+}
 
-    function get_content_(data) {
-        let result = {
-            video: {}
-        }
-        let collector = []
+const get_content_ = (data) => {
+    let result = {
+        video: {}
+    }
+    let smallestAudioFile
+    let videoFormats = ["144p", "240p", "480p", "720p", "1080p", "1440p", "2160p"]
 
-        function sort_coll_() {
-            collector.sort(function(a, b) {
-                let keyA = a.filesize,
-                    keyB = a.filesize
-                if (keyA < keyB) return -1
-                if (keyA > keyB) return 1
-                return 0
-            })
-        }
-
-        function builder_(cur, audio=false) {
-            let result = {
-                format: cur.format,
-                url: cur.url
-            }
-            if (audio) {
-                result.acodec = cur.acodec
-                result.ext = cur.ext
-            } else {
-                result.acodec = cur.acodec
-                result.vcodec = cur.vcodec
-                result.video_ext = cur.video_ext
-                result.fps = cur.fps
-            }
-            return result
-        }
-
-        for (let i = 0; i < data.length; i++) {
-            if (typeof data[i].asr !== 'undefined') {
-                if (data[i].asr) {
-                    if (data[i].resolution == "audio only" && data[i].asr == 48000) {
-                        collector.push(data[i])
-                        sort_coll_()
-                        result.audio = builder_(collector[0], audio=true)
-                    } else if (["360p", "720p"].includes(data[i].format_note)) {
-                        result.video[`q${data[i].format_note}`] = builder_(data[i])
-                    } 
-                } else if (["144p", "240p", "480p", "720p", "1080p", "1440p", "2160p"].includes(data[i].format_note)) {
+    for (let i = 0; i < data.length; i++) {
+        if (typeof data[i].asr !== 'undefined') {
+            if (data[i].asr) {
+                if (data[i].resolution == "audio only" && data[i].asr == 48000) {
+                    if (!smallestAudioFile || data[i].filesize < smallestAudioFile.filesize) {
+                        smallestAudioFile = data[i]
+                        result.audio = builder_(data[i], true)
+                    }
+                } else if (["360p", "720p"].includes(data[i].format_note)) {
                     result.video[`q${data[i].format_note}`] = builder_(data[i])
                 }
+            } else if (videoFormats.includes(data[i].format_note)) {
+                result.video[`q${data[i].format_note}`] = builder_(data[i])
             }
         }
-        return result
     }
+    return result
+}
 
+app.post('/getVideo', async (req, resp) => {
     try {
-        if (json_body.video_id) {
-            function response_call(result) {
-                return resp.send({
-                    success: true,
-                    body: result
-                })
-            }
-            redis.get(json_body.video_id, (error, result) => {
-                if (error) throw error
-                if (result !== null) {
-                    return response_call(JSON.parse(result))
-                } else {
-                    youtubedl(`https://www.youtube.com/watch?v=${json_body.video_id}`, {
-                        dumpSingleJson: true,
-                        noCheckCertificates: true,
-                        noWarnings: true,
-                        preferFreeFormats: true,
-                        addHeader: [
-                            'referer:youtube.com',
-                            'user-agent:googlebot'
-                        ]
+        const { video_id } = req.body
 
-                    }).then(output => {
-                        output.formats = get_content_(output.formats)
-                        let result = output
-                        redis.set(json_body.video_id, JSON.stringify(result), "ex", 600)
-                        return response_call(result)
-                    })
-                }
-            })
-        } else {
+        if (!video_id) {
             return resp.send({
                 success: false,
                 error: "video_id: null"
             })
         }
-    } catch (_) {
+
+        const redisResult = await getRedisAsync(video_id)
+
+        if (redisResult) {
+            return resp.send({
+                success: true,
+                body: JSON.parse(redisResult)
+            })
+        }
+
+        const output = await youtubedl(`https://www.youtube.com/watch?v=${video_id}`, {
+            dumpSingleJson: true,
+            noCheckCertificates: true,
+            noWarnings: true,
+            preferFreeFormats: true,
+            addHeader: [
+                'referer:youtube.com',
+                'user-agent:googlebot'
+            ]
+        })
+
+        const result = get_content_(output.formats)
+
+        await setRedisAsync(video_id, JSON.stringify(result), "ex", 600)
+
+        return resp.send({
+            success: true,
+            body: result
+        })
+    } catch (error) {
         return resp.send({
             success: false,
             error: "internal error function"
@@ -127,7 +123,7 @@ app.post('/getVideo', async (req, resp) => {
     }
 })
 
-app.get('*', async (req, resp) => {
+app.all('*', async (req, resp) => {
     return resp.status(404).json({
         success: false,
         error: "This route cannot be found",
